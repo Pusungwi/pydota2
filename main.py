5122ace92300ed47a1490af3a8d4c6618052d90d
Yi Yeon Jae <pusungwi@gmail.com>
2013-03-17 12:12:55 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# pydota2 v0.1
+# Dota 2 WebAPI in Python module
+# Author : Yi 'Pusungwi' Yeon Jae
+# License : New BSD License
+
+import os
+import json
+import urllib
+
+VERSION_NUMBER = 0.1
+
+if __name__ == "__main__":
+	print("pydota2 " + str(VERSION_NUMBER))
