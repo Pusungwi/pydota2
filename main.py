8899a34fbce004f2f17c88320801d3ab71a7c50a
Yi Yeon Jae <pusungwi@gmail.com>
2013-03-18 23:09:29 +0900

    add getHeroesDict(), getMatchDetailDict()

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import urllib.request, urllib.parse
 
 VERSION_NUMBER = 0.1
 WEBAPI_KEY = "E1005C7F35530B7E4E462FF4C5090253"
+
 #see http://wiki.teamfortress.com/wiki/WebAPI/GetMatchHistory
 GAME_MODE = ["All Pick", "Single Draft", "All Random", "Random Draft", "Captain's Draft", "Captain's Mode", "Death Mode",
  "Diretide", "Reverse Captain's Mode", "The Greeviling", "Tutorial", "Mid Only", "Least Played", "New Player Pool"]
@@ -26,8 +27,24 @@ class pyDota2:
 
 		self.webApiKey = apiKey
 
+	def getHeroesDict(self, language="en_us"):
+		print("getting Dota 2 Heroes...")
+		heroesUrl = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=" + self.webApiKey + "&language=" + language
+		heroesJSON = urllib.request.urlopen(heroesUrl)
+		heroesDecodedJSON = heroesJSON.read().decode("ascii")
+		resultDict = json.loads(heroesDecodedJSON)
+
+		return resultDict
+
 	def getMatchDetailDict(self, matchID):
 		#TODO: get match detail dict from match id
+		print("getting Dota 2 Match Detail...")
+		matchDetailUrl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=" + self.webApiKey + "&match_id=" + matchID
+		mDetailJSON = urllib.request.urlopen(matchDetailUrl)
+		mDetailDecodedJSON = mDetailJSON.read().decode("ascii")
+		resultDict = json.loads(mDetailDecodedJSON)
+
+		return resultDict
 
 	def getMatchHistoryDict(self, playerName=None, heroID=None, gameMode=None, skill=None, minDate=None, maxDate=None, 
 		minPlayers=None, accountID=None, leagueID=None, startMatchID=None, matchesRequested=None, tournamentGamesOnly=False):
@@ -57,17 +74,20 @@ class pyDota2:
 		if tournamentGamesOnly == True:
 			optionalUrl += "tournament_games_only=1&"	
 
-		print("getting Dota 2 Data...")
-		jsonUrl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?" + optionalUrl + "key=" + self.webApiKey
-		print(jsonUrl)
-		tRawJSON = urllib.request.urlopen(jsonUrl)
-		decodedRawJSON = tRawJSON.read().decode("ascii")
-		resultDict = json.loads(decodedRawJSON)["result"]
+		print("getting Dota 2 Match history...")
+		matchHistoryUrl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?" + optionalUrl + "key=" + self.webApiKey
+		mHistoryJSON = urllib.request.urlopen(matchHistoryUrl)
+		mHistoryDecodedJSON = mHistoryJSON.read().decode("ascii")
+		resultDict = json.loads(mHistoryDecodedJSON)
 	
 		return resultDict
 
 if __name__ == "__main__":
 	pydota = pyDota2(WEBAPI_KEY);
-	resultDict = pydota.getMatchHistoryDict(playerName="Gongwonchong", matchesRequested=1)
+	tmpMHistoryDict = pydota.getMatchHistoryDict(matchesRequested=1)
+	tmpMDetailDict = pydota.getMatchDetailDict("151800962")
+	tmpHeroesDict = pydota.getHeroesDict()
 
-	print(resultDict)
+	print(tmpMHistoryDict)
+	print(tmpMDetailDict)
+	print(tmpHeroesDict)
