3500cd365d40bf6d5a8e5f693726d16b71ded227
Yi Yeon Jae <pusungwi@gmail.com>
2013-03-19 13:25:28 +0900

    add getRaritiesDict(), getMatchHistoryDictBySequenceNum(), getTeamInfoDictByTeamID(), getLiveLeagueGamesDict() add test code in __main__ method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# pydota2 v0.1
+# pydota2 v0.2
 # Dota 2 WebAPI in Python module
 # Author : Yi 'Pusungwi' Yeon Jae
 # License : New BSD License
@@ -9,10 +9,10 @@ import os
 import json
 import urllib.request, urllib.parse
 
-VERSION_NUMBER = 0.1
+VERSION_NUMBER = 0.2
 WEBAPI_KEY = "E1005C7F35530B7E4E462FF4C5090253"
 
-#see http://wiki.teamfortress.com/wiki/WebAPI/GetMatchHistory
+
 GAME_MODE = ["All Pick", "Single Draft", "All Random", "Random Draft", "Captain's Draft", "Captain's Mode", "Death Mode",
  "Diretide", "Reverse Captain's Mode", "The Greeviling", "Tutorial", "Mid Only", "Least Played", "New Player Pool"]
 
@@ -28,6 +28,7 @@ class pyDota2:
 		self.webApiKey = apiKey
 
 	def getHeroesDict(self, language="en_us"):
+		#A list of heroes within Dota 2.
 		print("getting Dota 2 Heroes...")
 		heroesUrl = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=" + self.webApiKey + "&language=" + language
 		heroesJSON = urllib.request.urlopen(heroesUrl)
@@ -36,8 +37,58 @@ class pyDota2:
 
 		return resultDict
 
+	def getRaritiesDict(self, language="en_us"):
+		#Dota 2 item rarity list.
+		print("getting Dota 2 Rarities...")
+		raritiesUrl = "http://api.steampowered.com/IEconDOTA2_570/GetRarities/v1/?key=" + self.webApiKey + "&language=" + language
+		raritiesJSON = urllib.request.urlopen(raritiesUrl)
+		raritiesDecodedJSON = raritiesJSON.read().decode("ascii")
+		resultDict = json.loads(raritiesDecodedJSON)
+
+		return resultDict
+
+	def getMatchHistoryDictBySequenceNum(self, startAtMatchSeqNum, matchesRequested=25):
+		#A list of matches ordered by their sequence num.
+		print("getting Match History Dict By Sequence Number...")
+		mHistorySNumUrl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/v0001/?key=" + self.webApiKey + "&start_at_match_seq_num=" + str(startAtMatchSeqNum) + "&matches_requested=" + str(matchesRequested)
+		mHistorySNumJSON = urllib.request.urlopen(mHistorySNumUrl)
+		mHistorySNumDecodedJSON = mHistorySNumJSON.read().decode("ascii")
+		resultDict = json.loads(mHistorySNumDecodedJSON)
+
+		return resultDict
+
+	def getTeamInfoDictByTeamID(self, startAtTeamID=0, teamRequested=10):
+		#A list of all the teams set up in-game.
+		print("getting Team Info Dict By TeamID...")
+		tTeamIDUrl = "https://api.steampowered.com/IDOTA2Match_570/GetTeamInfoByTeamID/v001/?key=" + self.webApiKey + "&start_at_team_id=" + str(startAtTeamID) + "&teams_requested=" + str(teamRequested)
+		tTeamIDJSON = urllib.request.urlopen(tTeamIDUrl)
+		tTeamIDDecodedJSON = tTeamIDJSON.read().decode("ascii")
+		resultDict = json.loads(tTeamIDDecodedJSON)
+
+		return resultDict
+
+	def getLiveLeagueGamesDict(self):
+		#A list of in-progress league matches, as well as details of that match as it unfolds.
+		print("getting Dota 2 Live League Gaming...")
+		lLeagueUrl = "https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001/?key=" + self.webApiKey
+		lLeagueJSON = urllib.request.urlopen(lLeagueUrl)
+		lLeagueDecodedJSON = lLeagueJSON.read().decode("ascii")
+		resultDict = json.loads(lLeagueDecodedJSON)
+
+		return resultDict
+
+	def getLeagueListingDict(self, language="en_us"):
+		#Information about DotaTV-supported leagues.
+		print("getting Dota 2 League Listing...")
+		lListingUrl = "https://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v0001/?key=" + self.webApiKey + "&language=" + language
+		lListingJSON = urllib.request.urlopen(lListingUrl)
+		lListingDecodedJSON = lListingJSON.read().decode("ascii")
+		resultDict = json.loads(lListingDecodedJSON)
+
+		return resultDict
+
 	def getMatchDetailDict(self, matchID):
-		#TODO: get match detail dict from match id
+		#Information about a particular match.
 		print("getting Dota 2 Match Detail...")
 		matchDetailUrl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=" + self.webApiKey + "&match_id=" + matchID
 		mDetailJSON = urllib.request.urlopen(matchDetailUrl)
@@ -48,6 +99,8 @@ class pyDota2:
 
 	def getMatchHistoryDict(self, playerName=None, heroID=None, gameMode=None, skill=None, minDate=None, maxDate=None, 
 		minPlayers=None, accountID=None, leagueID=None, startMatchID=None, matchesRequested=None, tournamentGamesOnly=False):
+		#A list of matches, filterable by various parameters.
+		#for more detail information. see http://wiki.teamfortress.com/wiki/WebAPI/GetMatchHistory
 		optionalUrl = ""
 		if playerName != None:
 			optionalUrl += "player_name=" + urllib.parse.quote(playerName) + "&"
@@ -87,7 +140,17 @@ if __name__ == "__main__":
 	tmpMHistoryDict = pydota.getMatchHistoryDict(matchesRequested=1)
 	tmpMDetailDict = pydota.getMatchDetailDict("151800962")
 	tmpHeroesDict = pydota.getHeroesDict()
+	tmpLListingDict = pydota.getLeagueListingDict()
+	tmpLLeagueDict = pydota.getLiveLeagueGamesDict()
+	tmpTInfoDict = pydota.getTeamInfoDictByTeamID()
+	tmpMDetailSNDict = pydota.getMatchHistoryDictBySequenceNum("10")
+	tmpRaritiesDict = pydota.getRaritiesDict()
 
 	print(tmpMHistoryDict)
 	print(tmpMDetailDict)
 	print(tmpHeroesDict)
+	print(tmpLListingDict)
+	print(tmpLLeagueDict)
+	print(tmpTInfoDict)
+	print(tmpMDetailSNDict)
+	print(tmpRaritiesDict)
