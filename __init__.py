8a0e15856904ff13ac144a201229a26c85babf1d
Yi Yeon Jae <pusungwi@gmail.com>
2013-03-26 23:33:05 +0900

    ready for converting module system

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+    pydota2
+    ~~~~~~
+
+    A module for Dota 2 WebAPI.
+
+    :copyright: (c) 2013 by Yi 'Pusungwi' Yeon Jae
+    :license: New BSD License, see LICENSE for more details.
+"""
+
+from . import main
+
+__version__ = '0.2'
