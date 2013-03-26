ea0217bf891b9c62781adfc9c3005e83791087ce
Yi Yeon Jae <pusungwi@gmail.com>
2013-03-26 23:35:55 +0900

    add more module file / remove

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+    pydota2
+    ~~~~~~
+
+    A module for Dota 2 WebAPI.
+
+    :copyright: (c) 2013 by Yi 'Pusungwi' Yeon Jae
+    :license: New BSD License, see LICENSE for more details.
+"""
+
+print("test")
