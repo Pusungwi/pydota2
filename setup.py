30aa030c6a2c4411170a8455bd84cf01472d5693
Pusungwi <pusungwi@gmail.com>
2014-02-08 23:36:49 +0900

    0.4 re-arranged all method in changed WebAPI (2014/02/08)

--- setup.py
+++ setup.py
@@ -5,8 +5,38 @@
 
     A module for Dota 2 WebAPI.
 
-    :copyright: (c) 2013 by Yi 'Pusungwi' Yeon Jae
+    :copyright: (c) 2014 by Yi 'Pusungwi' Yeon Jae
     :license: New BSD License, see LICENSE for more details.
 """
 
-print("test")
+from distutils.core import setup
+
+import os
+
+def get_build():
+	path = "./.build"
+	
+	if os.path.exists(path):
+		fp = open(path, "r")
+		build = eval(fp.read())
+		if os.path.exists("./.increase_build"):
+			build += 1
+		fp.close()
+	else:
+		build = 1
+	
+	fp = open(path, "w")
+	fp.write(str(build))
+	fp.close()
+	
+	return str(build)
+
+setup(name = "pydota2",
+      version = "0.3." + get_build(),
+      author = "Yi Yeon Jae <pusungwi@gmail.com>",
+	  description = "A Python module for DOTA 2 WebAPI",
+      author_email = "pusungwi@gmail.com",
+      url = "http://github.com/pusungwi/pydota2/",
+      py_modules = ("pydota2",),
+	  license = "BSD"
+	)
